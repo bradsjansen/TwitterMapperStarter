b91bfee465fcb97e2d23efd1cf73dc7dfb49c277
bradsjansen <113173037+bradsjansen@users.noreply.github.com>
2024-10-10 15:32:19 +0200

    API Keys and Access Tokens Changed
    Changed the projects default API keys and Access Tokens to the new ones generated for my project.

--- TwitterMapperStarter/src/twitter/LiveTwitterSource.java
+++ TwitterMapperStarter/src/twitter/LiveTwitterSource.java
@@ -44,10 +44,10 @@ public class LiveTwitterSource extends TwitterSource {
     // Create ConfigurationBuilder and pass in necessary credentials to authorize properly, then create TwitterStream.
     private void initializeTwitterStream() {
         ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setOAuthConsumerKey("Yt8nu9oBskoRMnKYUWp22BDam")
-                .setOAuthConsumerSecret("F7MaPDZs1YYBBTsxvgTV9hYikuD9ydTv5NOcxtAXCWcfUhVgcc")
-                .setOAuthAccessToken("910973574550192128-CW2RGcxU4COOEYiwbM1G1qewrB5iFWO")
-                .setOAuthAccessTokenSecret("2Rr5xYnH8odDCFlyhWE5fzA5Y9JYVGqPECijjmzaUuuzt");
+        cb.setOAuthConsumerKey("XX9em1pXb5RnpIh8RmsjrHnAW")
+                .setOAuthConsumerSecret("vjmlbv9BYOKzqv8GSEwR8t0SuhbY6amobxdlQSRNvh6RlcgSJO")
+                .setOAuthAccessToken("2984432830-VuYzsX96L9u87DbMHZTyAeuhbpLCh0KcGVHxUnF")
+                .setOAuthAccessTokenSecret("8y80Rtrbl11fcDO5iezNlOjEAu5iEviHpqbDzqBRAU33W");
 
         // Pass the ConfigurationBuilder in when constructing TwitterStreamFactory.
         twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
